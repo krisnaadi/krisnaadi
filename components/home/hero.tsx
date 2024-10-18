70669c4d74bd41fb56ad687362ed5eee6fdc01ef
krisnaadiSuastika <krisnaadi@kbsdigital.co.id>
2024-10-18 10:50:12 +0800

    init

---
+++ components/home/hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <section>
+        <h1>Full-stack Developer</h1>
+        <p>I strive to write clean, maintainable, and understandable code, ensuring a smooth and enjoyable development experience.</p>
+        <p className="grid">
+          <button className="outline">Github</button>
+          <button className="outline">Linkedin</button>
+        </p>
+    </section>
+  );
+}
---
+++ components/home/work.tsx
@@ -0,0 +1,41 @@
+export default function Work() {
+    return (
+        <section>
+            <h2>Work</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>2024 - Present</td>
+                        <td>Perumda Kerthi Bali Santhi</td>
+                        <td>Full-stack Developer</td>
+                    </tr>
+                    <tr>
+                        <td>2022 - 2023</td>
+                        <td>Djoin</td>
+                        <td>Senior Software Engineer</td>
+                    </tr>
+                    <tr>
+                        <td>2021 - 2022</td>
+                        <td>Career Break</td>
+                        <td>Full-time parenting</td>
+                    </tr>
+                    <tr>
+                        <td>2019 - 2021</td>
+                        <td>Avatar Solution</td>
+                        <td>Back-end Developer</td>
+                    </tr>
+                    <tr>
+                        <td>2018 - 2019</td>
+                        <td>Avatar Solution</td>
+                        <td>Database Administrator</td>
+                    </tr>
+                    <tr>
+                        <td>2014 - 2018</td>
+                        <td>Avatar Solution</td>
+                        <td>C# Programmer</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    );
+}
