55734d514417e28e97247201dccedd2226a16ab3
krisnaadiSuastika <krisnaadi@kbsdigital.co.id>
2024-10-18 10:58:38 +0800

    feat: change indent

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -1,22 +1,28 @@
 export default function About() {
-    return (
-        <section>
-            <h2>About</h2>
-            <p>Hello! I'm Krisnaadi, a full-stack developer. More than 5 year experience</p>
-            <ul>
-                <li>
-                    <h3>Front-end</h3>
-                    <p>Typescript / Nuxtjs / Nextjs</p>
-                </li>
-                <li>
-                    <h3>Styles</h3>
-                    <p>Tailwindcss / Bootstrap / Picocss</p>
-                </li>
-                <li>
-                    <h3>Back-end</h3>
-                    <p>PHP / Laravel / Golang / PostgreSQL / MySQL / Redis / Docker / Sqlite</p>
-                </li>
-            </ul>
-        </section>
-    );
+  return (
+    <section>
+      <h2>About</h2>
+      <p>
+        Hello! I'm Krisnaadi, a full-stack developer. More than 5 year
+        experience
+      </p>
+      <ul>
+        <li>
+          <h3>Front-end</h3>
+          <p>Typescript / Nuxtjs / Nextjs</p>
+        </li>
+        <li>
+          <h3>Styles</h3>
+          <p>Tailwindcss / Bootstrap / Picocss</p>
+        </li>
+        <li>
+          <h3>Back-end</h3>
+          <p>
+            PHP / Laravel / Golang / PostgreSQL / MySQL / Redis / Docker /
+            Sqlite
+          </p>
+        </li>
+      </ul>
+    </section>
+  );
 }
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,22 +1,19 @@
 export default function Header() {
-    return (
-        <header className="container">
-          <nav>
-            <ul>
-              <li>
-                krisnaadi
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a>About</a>
-              </li>
-              <li>
-                <a>Projects</a>
-              </li>
-            </ul>
-          </nav>
-        </header>
-    )
-  }
-  
+  return (
+    <header className="container">
+      <nav>
+        <ul>
+          <li>krisnaadi</li>
+        </ul>
+        <ul>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Projects</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
