c0514b40b81ba80654af586855d9283727f846ea
krisnaadiSuastika <krisnaadi@kbsdigital.co.id>
2024-10-21 10:05:42 +0800

    feat connect to supabase

--- components/home/work.tsx
+++ components/home/work.tsx
@@ -1,39 +1,21 @@
-export default function Work() {
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Work() {
+  const supabase = createClient();
+  const { data: works } = await supabase.from("works").select();
+  console.log(works);
   return (
     <section>
       <h2>Work</h2>
       <table>
         <tbody>
-          <tr>
-            <td>2024 - Present</td>
-            <td>Perumda Kerthi Bali Santhi</td>
-            <td>Full-stack Developer</td>
-          </tr>
-          <tr>
-            <td>2022 - 2023</td>
-            <td>Djoin</td>
-            <td>Senior Software Engineer</td>
-          </tr>
-          <tr>
-            <td>2021 - 2022</td>
-            <td>Career Break</td>
-            <td>Full-time parenting</td>
-          </tr>
-          <tr>
-            <td>2019 - 2021</td>
-            <td>Avatar Solution</td>
-            <td>Back-end Developer</td>
-          </tr>
-          <tr>
-            <td>2018 - 2019</td>
-            <td>Avatar Solution</td>
-            <td>Database Administrator</td>
-          </tr>
-          <tr>
-            <td>2014 - 2018</td>
-            <td>Avatar Solution</td>
-            <td>C# Programmer</td>
-          </tr>
+          {works?.map((work) => (
+            <tr key={work.id}>
+              <td>{work.years}</td>
+              <td>{work.company}</td>
+              <td>{work.position}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </section>
